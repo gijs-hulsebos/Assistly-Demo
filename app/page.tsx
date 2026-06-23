7a5abf94f255fb721655492ac88a8d332395757f
Gijs Hulsebos <gijs@gijshulsebos.com>
2026-06-23 23:30:47 +0200

    style: update background color to white on home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-[#FAFAFA] relative overflow-hidden">
+    <div className="bg-white relative overflow-hidden">
       {/* Abstract Grid Background */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none"></div>
 
